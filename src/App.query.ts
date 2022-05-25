6f6504ed0805dc6fc52e3fc5339c2232a966e2d3
pyuung <kyuungah@gmail.com>
2022-05-26 01:43:31 +0900

    feat: :fire: boilerplate V1

---
+++ src/App.query.ts
@@ -0,0 +1,12 @@
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { defaultBaseQuery } from './api/baseQuerySetting'
+
+export const appQuery = createApi({
+  reducerPath: 'appQuery',
+  baseQuery: defaultBaseQuery,
+  tagTypes: ['app'],
+
+  endpoints: (builder) => ({}),
+})
+
+export const {} = appQuery
---
+++ src/api/baseQuerySetting/index.ts
@@ -0,0 +1,11 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const BASE_URL = ''
+
+export const defaultBaseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  credentials: 'omit',
+})
